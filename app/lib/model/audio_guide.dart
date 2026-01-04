99807b17d51c853bc5da56714f2cc0c0353086bf
hengputh52 <harrystyles10203040@gmail.com>
2026-01-05 00:59:28 +0700

    update language switching feature

--- app/lib/model/audio_guide.dart
+++ app/lib/model/audio_guide.dart
@@ -1,21 +1,20 @@
-import 'language.dart';
 
 class AudioGuide {
-  final Language language;
+  //final Language language;
   final String title;
   //final String description;
   final String audioPath;
 
   AudioGuide({
-    required this.language,
+    
     required this.title,
     //required this.description,
     required this.audioPath,
   });
 
-  factory AudioGuide.fromJson(String langKey, Map<String, dynamic> json) {
+  factory AudioGuide.fromJson(Map<String, dynamic> json) {
     return AudioGuide(
-      language: Language.values.byName(langKey),
+      
       title: json['title'],
       //description: json['description'],
       audioPath: json['audio'],
